e7dddb7f6ba4c5b8cb1796dc680cf864c2a041ce
vladfedchenko <vladfedchenko@gmail.com>
2018-04-03 12:07:02 +0200

    Mixed population dataset support.

--- src/scripts/nn/prepare_trace_dataset.py
+++ src/scripts/nn/prepare_trace_dataset.py
@@ -30,12 +30,18 @@ def main():
                         "--window_group_size",
                         type=int,
                         help="result dataset column number",
-                        default=3)
+                        default=5)
     parser.add_argument("-o",
                         "--output",
                         type=str,
                         help="output file name. \"dataset.csv\" otherwise",
                         default="dataset.csv")
+    parser.add_argument("-cs",
+                        "--class_separator",
+                        help=("class separation value, basically number of items in first population if population is " 
+                              "mixed. If passed then every row will have class label"),
+                        type=int,
+                        default=None)
     args = parser.parse_args()
 
     input_df = pd.read_csv(args.input, header=None, names=["from_start", "from_prev", "id"])
@@ -44,6 +50,10 @@ def main():
     max_time = input_df.from_start.max()
 
     time_processed = input_df.from_start.min()
+
+    ids_class = None
+    if args.class_separator is not None:
+        ids_class = list(map(lambda x: int(x > args.class_separator), ids))
 
     window_list = []
     with tqdm(total=max_time - time_processed, desc="Time processed", unit="unit(s)") as pbar:
@@ -67,6 +77,9 @@ def main():
 
                 if len(window_list) == args.window_group_size:
                     data_matrix = np.matrix(window_list).T
+                    if ids_class is not None:
+                        ids_class_matrix = np.matrix([ids_class]).T
+                        data_matrix = np.concatenate((ids_class_matrix, data_matrix), axis=1)
                     write_batch(f, data_matrix)
 
                 time_processed += args.window_size
