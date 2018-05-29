b705316f91f6c0755fcee0ab5444afc66c03f95c
vladfedchenko <vladfedchenko@gmail.com>
2018-05-29 18:28:27 +0200

    Split evaluation script added

--- src/scripts/nn/evaluate_split_error.py
+++ src/scripts/nn/evaluate_split_error.py
@@ -1,6 +1,3 @@
-"""
-This script is created to evaluate the error on generated traces using feedforward neural network.
-"""
 import pandas as pd
 import numpy as np
 from neural_nets import TorchFeedforwardNN
@@ -8,7 +5,6 @@ import torch
 import torch.utils.data
 import argparse
 from tqdm import tqdm
-import pickle
 import os
 
 
@@ -40,6 +36,11 @@ def main():
                         type=int,
                         help="iterations to do",
                         default=1000)
+    parser.add_argument("-ti",
+                        "--train_iterations",
+                        type=int,
+                        help="iterations to train NN",
+                        default=10)
     parser.add_argument("-l",
                         "--learning_rate",
                         type=float,
@@ -60,16 +61,6 @@ def main():
                         type=float,
                         help="train - validation split fraction",
                         default=0.8)
-    parser.add_argument("-pf",
-                        "--pickle_file",
-                        type=int,
-                        help="pickle file index to dump neural network state after learning",
-                        default=None)
-    parser.add_argument("-uf",
-                        "--unpickle_file",
-                        type=int,
-                        help="pickle file index to restore neural network state from at the beginning",
-                        default=None)
     parser.add_argument("-ml",
                         "--middle_layers",
                         type=int,
@@ -92,9 +83,6 @@ def main():
                         "--input_has_labels",
                         help="pass this is input has class label. Needed for optimal predictor evaluation",
                         action="store_true")
-    parser.add_argument("--seed",
-                        help="seed for item sampling",
-                        type=int)
     parser.add_argument("-fc",
                         "--force_cpu",
                         help="force cpu execution for PyTorch",
@@ -104,82 +92,69 @@ def main():
     if not os.path.exists(args.directory):
         os.makedirs(args.directory)
 
-    if args.seed:
-        torch.manual_seed(args.seed)
-        np.random.seed(args.seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed(args.seed)
-
-    data = pd.read_csv(args.input, header=None)
-
-    if args.sample:
-        data = data.sample(n=args.sample)
-
-    n = len(data)
-    train_size = n * args.train_validation_split
-
-    train_data = data.sample(n=int(train_size))
-    valid_data = data.drop(train_data.index)
-
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     if args.force_cpu:
         device = "cpu"
     print("Running on: {0}".format(device))
 
-    if args.unpickle_file is not None:
-        filename = "nn_{0}.p".format(args.unpickle_file)
-        filename = os.path.join(args.directory, filename)
-        with open(filename, "rb") as unpickle_file:
-            nn = pickle.load(unpickle_file)
-    else:
-        layers = [data.shape[1] - 2] + ([args.middle_layer_neurons] * args.middle_layers) + [1]
-        nn = TorchFeedforwardNN(layers,
-                                hidden_activation=args.hidden_activation,
-                                out_activation=args.out_activation)
-        if torch.cuda.is_available():
-            nn.to(device)
-
-    inp_train = np.matrix(train_data.iloc[:, 1:train_data.shape[1] - 1])
-    outp_train = np.matrix(train_data.iloc[:, train_data.shape[1] - 1:train_data.shape[1]])
-
-    inp_valid = np.matrix(valid_data.iloc[:, 1:valid_data.shape[1] - 1])
-    outp_valid = np.matrix(valid_data.iloc[:, valid_data.shape[1] - 1:valid_data.shape[1]])
-
-    optim_err = calc_aver_error(inp_valid, outp_valid, args.input_has_labels)
-    optim_err_train = calc_aver_error(inp_train, outp_train, args.input_has_labels)
-
-    inp_train = torch.from_numpy(inp_train)
-    outp_train = torch.from_numpy(outp_train)
-    inp_valid = torch.from_numpy(inp_valid)
-    outp_valid = torch.from_numpy(outp_valid)
-
-    if torch.cuda.is_available():
-        inp_train = inp_train.to(device)
-        outp_train = outp_train.to(device)
-        inp_valid = inp_valid.to(device)
-        outp_valid = outp_valid.to(device)
+    data_full = pd.read_csv(args.input, header=None)
 
     error_file = os.path.join(args.directory, "error.txt")
     with open(error_file, "w") as f:
-        f.write("{} {}\n".format(optim_err_train, optim_err))
-        for _ in tqdm(range(args.iterations), desc="Running iterations"):
-            train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(inp_train, outp_train),
-                                                       batch_size=args.mini_batch,
-                                                       shuffle=True)
-            for inp, target in tqdm(train_loader, desc="Running minibatches"):
-                nn.backpropagation_learn(inp, target, args.learning_rate, show_progress=True, stochastic=False)
+        for seed in tqdm(range(args.iterations), desc="Running iterations"):
+            torch.manual_seed(seed)
+            np.random.seed(seed)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed(seed)
+
+            data = data_full
+            if args.sample:
+                data = data.sample(n=args.sample)
+
+            n = len(data)
+            train_size = n * args.train_validation_split
+
+            train_data = data.sample(n=int(train_size))
+            valid_data = data.drop(train_data.index)
+
+            layers = [data.shape[1] - 2] + ([args.middle_layer_neurons] * args.middle_layers) + [1]
+            nn = TorchFeedforwardNN(layers,
+                                    hidden_activation=args.hidden_activation,
+                                    out_activation=args.out_activation)
+            if torch.cuda.is_available():
+                nn.to(device)
+
+            inp_train = np.matrix(train_data.iloc[:, 1:train_data.shape[1] - 1])
+            outp_train = np.matrix(train_data.iloc[:, train_data.shape[1] - 1:train_data.shape[1]])
+            inp_valid = np.matrix(valid_data.iloc[:, 1:valid_data.shape[1] - 1])
+            outp_valid = np.matrix(valid_data.iloc[:, valid_data.shape[1] - 1:valid_data.shape[1]])
+
+            optim_err = calc_aver_error(inp_valid, outp_valid, args.input_has_labels)
+            optim_err_train = calc_aver_error(inp_train, outp_train, args.input_has_labels)
+
+            inp_train = torch.from_numpy(inp_train)
+            outp_train = torch.from_numpy(outp_train)
+            inp_valid = torch.from_numpy(inp_valid)
+            outp_valid = torch.from_numpy(outp_valid)
+
+            if torch.cuda.is_available():
+                inp_train = inp_train.to(device)
+                outp_train = outp_train.to(device)
+                inp_valid = inp_valid.to(device)
+                outp_valid = outp_valid.to(device)
+
+            for _ in tqdm(range(args.train_iterations), desc="Training NN"):
+                train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(inp_train, outp_train),
+                                                           batch_size=args.mini_batch,
+                                                           shuffle=True)
+                for inp, target in tqdm(train_loader, desc="Running minibatches"):
+                    nn.backpropagation_learn(inp, target, args.learning_rate, show_progress=True, stochastic=False)
 
             train_err = nn.evaluate(inp_train, outp_train)
             valid_err = nn.evaluate(inp_valid, outp_valid)
 
-            f.write("{} {}\n".format(train_err, valid_err))
+            f.write("{} {} {} {}\n".format(optim_err_train, optim_err, train_err, valid_err))
             f.flush()
-
-    if args.pickle_file is not None:
-        filename = "nn_{0}.p".format(args.pickle_file)
-        filename = os.path.join(args.directory, filename)
-        with open(filename, "wb") as pickle_file:
-            pickle.dump(nn, pickle_file)
 
 
 if __name__ == "__main__":
