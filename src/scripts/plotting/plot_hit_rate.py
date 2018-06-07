9f57743a56d6ba97486673ecc9f1f1b3a64fc4a6
vladfedchenko <vladfedchenko@gmail.com>
2018-06-07 11:44:06 +0200

    Hit rate plotting added

---
+++ src/scripts/plotting/plot_hit_rate.py
@@ -0,0 +1,64 @@
+"""
+This script is created to plot cache hit rate.
+"""
+import argparse
+import matplotlib.pyplot as plt
+import os
+from matplotlib.pyplot import cm
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory",
+                        type=str,
+                        help="directory with data files")
+    parser.add_argument("-x",
+                        "--size_x",
+                        type=int,
+                        help="plot size in x axis",
+                        default=10)
+    parser.add_argument("-y",
+                        "--size_y",
+                        type=int,
+                        help="plot size in y axis",
+                        default=10)
+    args = parser.parse_args()
+
+    txts = []
+    for root, dirs, files in os.walk(args.directory):
+        txts_tmp = [x for x in files if x.endswith(".txt")]
+        txts += txts_tmp
+
+    txts = sorted([os.path.join(args.directory, x) for x in txts])
+
+    colors = cm.rainbow(np.linspace(0, 1, len(txts)))
+
+    fig = plt.figure(1, figsize=(args.size_x, args.size_y))
+    fig.suptitle("Hit rate evaluation")
+
+    plotted = []
+    for file_name, col in zip(txts, colors):
+        with open(file_name, "r") as f:
+            lines = [x.split() for x in f.readlines()]
+            line_name = lines[0][0]
+
+            lines = lines[1:]
+
+            cache_sizes = [int(x[0]) for x in lines]
+            cache_hits = [float(x[1]) for x in lines]
+
+            pl_line, = plt.plot(cache_sizes, cache_hits, c=col, label=line_name)
+            plotted.append(pl_line)
+
+    plt.xlabel("Cache size")
+    plt.ylabel("Hit ratio")
+
+    plt.legend(handles=plotted)
+
+    plot_name = os.path.join(args.directory, "cache_hit_plot.png")
+    plt.savefig(plot_name)
+
+
+if __name__ == "__main__":
+    main()
