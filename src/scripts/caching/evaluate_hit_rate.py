e013a20d4fa6099196821603abb2b0f9d1fd11b3
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2018-06-30 16:27:00 +0200

    HIt rate evaluation updated

--- src/scripts/caching/evaluate_hit_rate.py
+++ src/scripts/caching/evaluate_hit_rate.py
@@ -7,9 +7,10 @@ import pandas as pd
 import pickle
 from caching import *
 from caching.abstract_cache import AbstractCache
+import json
 
 
-def eval_cache_hit(cache: AbstractCache, trace: pd.DataFrame) -> float:
+def eval_cache_hit(cache: AbstractCache, trace: pd.DataFrame, cold_start_skip: int) -> float:
     """
     Evaluate cache hit on some trace.
     :param cache: Cache object.
@@ -18,7 +19,9 @@ def eval_cache_hit(cache: AbstractCache, trace: pd.DataFrame) -> float:
     """
     requests = 0
     hits = 0.0
-    for i, row in tqdm(trace.iterrows(), desc="Running trace", total=len(trace)):
+    i = 0
+    for _, row in tqdm(trace.iterrows(), desc="Running trace", total=len(trace)):
+
         requests += 1
         if cache.request_object(row.id, 1, row.from_start):
             hits += 1.0
@@ -39,9 +42,21 @@ def main():
     parser.add_argument("max_cache",
                         type=int,
                         help="max cache size")
+    parser.add_argument("cache_type",
+                        type=str,
+                        help="type of caching policy to use")
     parser.add_argument("output",
                         help="output file name",
                         type=str)
+    parser.add_argument("-cd",
+                        "--cache_descriptor",
+                        type=str,
+                        help="descriptor to construct cache object")
+    parser.add_argument("-css",
+                        "--cold_start_skip",
+                        help="number of requests to skip when evaluating hit rate",
+                        type=int,
+                        default=0)
     args = parser.parse_args()
 
     input_df = pd.read_csv(args.input, header=None, names=["from_start", "from_prev", "id"])
@@ -52,19 +67,34 @@ def main():
         cur_size = args.starting_cache
         with open(args.output, 'w') as f:
             while cur_size <= args.max_cache:
-                # Feel free to change the type of cache to evaluate
 
-                # with open("cache_nn_half_win/case2_lw/nn_2.p", "rb") as unpickle_file:
-                #     nn = pickle.load(unpickle_file)
-                # cache = FeedforwardNNCacheFullTorch(cur_size, nn, 4, 10_000_000, 5)
+                desc = None
+                if args.c is not None:
+                    with open(args.cache_descriptor) as f:
+                        desc = json.load(f)
 
-                cache = FutureInfoCache(cur_size, "data/datasets/16_modified_traces/real_500_mod_trace.csv")
+                if args.cache_type == "nn":
+                    with open(desc["nn_location"], "rb") as unpickle_file:
+                        nn = pickle.load(unpickle_file)
+                    cache = FeedforwardNNCacheFullTorch(cur_size,
+                                                        nn,
+                                                        int(desc["counter_num"]),
+                                                        float(desc["time_window"]),
+                                                        int(desc["update_sample_size"]))
 
-                # cache = LRUCache(cur_size)
+                elif args.cache_type == "future":
+                    cache = FutureInfoCache(cur_size, desc["mod_trace_path"])
 
-                # cache = ARCache(cur_size)
+                elif args.cache_type == "lru":
+                    cache = LRUCache(cur_size)
 
-                hit_rate = eval_cache_hit(cache, input_df)
+                elif args.cache_type == "arc":
+                    cache = ARCache(cur_size)
+
+                else:
+                    raise Exception("Unidentified error type")
+
+                hit_rate = eval_cache_hit(cache, input_df, args.cold_start_skip)
                 f.write(f"{cur_size} {hit_rate}\n")
                 f.flush()
 
