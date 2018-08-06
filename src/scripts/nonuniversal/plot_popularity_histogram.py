740602182364729cd871b9dc3e8908abe4d57cc3
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2018-08-06 13:56:41 +0200

    Popularity plotting script added

---
+++ src/scripts/nonuniversal/plot_popularity_histogram.py
@@ -0,0 +1,15 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+with open("data/requests_item_pop.csv", "r") as f:
+    lines = [int(x.split(",")[1]) for x in f.readlines()]
+
+    fig = plt.figure(1, figsize=(5, 5))
+    fig.suptitle("Item popularity", fontsize=14)
+
+    plt.hist(lines, bins=100)
+
+    plt.xlabel("Number of requests", fontsize=14)
+
+    plt.savefig("item_popularity.png")
