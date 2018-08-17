dadf90fff9e4b0131e1aae4796606743088c60ef
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2018-08-17 14:04:17 +0200

    A new script added

---
+++ src/scripts/nonuniversal/extract_gz.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import io
+from natsort import natsorted
+import tqdm
+
+dirname = "data/log_share"
+
+fl = natsorted(os.listdir(dirname))
+
+total_write = 100_000_000
+i = 0
+with tqdm.tqdm(total=total_write) as prog_bar:
+    with open("data/real_trace_2.csv", "w") as f:
+        for fn in fl:
+            if not fn.endswith(".gz"):
+                continue
+            fullfn = os.path.join(dirname, fn)
+
+            p = subprocess.Popen(["zcat", fullfn], stdout=subprocess.PIPE)
+            fh = io.BytesIO(p.communicate()[0])
+            assert p.returncode == 0
+
+            for line in fh:
+                arr = line.split()
+                f.write("{}, {}, {}\n".format(line[0], 1, line[1]))
+                prog_bar.update(1)
+                i += 1
+                if i == total_write:
+                    break
+
+            if i == total_write:
+                break
