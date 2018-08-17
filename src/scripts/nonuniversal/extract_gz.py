f5526c2fc3adacb9ea2c0e85d5301bb3d0ced62b
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2018-08-17 14:37:33 +0200

    Fixes

--- src/scripts/nonuniversal/extract_gz.py
+++ src/scripts/nonuniversal/extract_gz.py
@@ -23,7 +23,7 @@ with tqdm.tqdm(total=total_write) as prog_bar:
 
             for line in fh:
                 arr = line.split()
-                f.write("{}, {}, {}\n".format(line[0], 1, line[1]))
+                f.write("{}, {}, {}\n".format(arr[0], 1, arr[1]))
                 prog_bar.update(1)
                 i += 1
                 if i == total_write:
