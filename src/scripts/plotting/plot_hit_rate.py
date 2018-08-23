75d8c49d1ccc6e2e3ede2291f78bb62ed534b1ab
vladfedchenko <vladfedchenko@gmail.com>
2018-08-23 22:50:54 +0200

    plotting improvements

--- src/scripts/plotting/plot_hit_rate.py
+++ src/scripts/plotting/plot_hit_rate.py
@@ -43,6 +43,10 @@ def main():
                'D', 'd', '|', '4', '_']
 
     plotted = []
+    tmp_map = {}
+    txts = ["nn_3.txt", "nn_10.txt", "nn_50.txt", "nn_200.txt", "nn_1000.txt"]
+    txts = [os.path.join(args.directory, x) for x in txts]
+
     for i, (file_name, col) in enumerate(zip(txts, colors)):
         with open(file_name, "r") as f:
             lines = f.readlines()
@@ -54,11 +58,33 @@ def main():
             cache_sizes = [int(x[0]) for x in lines]
             cache_hits = [float(x[1]) for x in lines]
 
+            for s, h in zip(cache_sizes, cache_hits):
+                if i == 0:
+                    tmp_map[s] = h
+                elif tmp_map[s] < h:
+                    tmp_map[s] = h
+
+    for i, (file_name, col) in enumerate(zip(txts, colors)):
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+            line_name = lines[0]
+            lines = lines[1:]
+
+            lines = [x.split() for x in lines]
+
+            cache_sizes = [int(x[0]) for x in lines]
+            cache_hits = [float(x[1]) for x in lines]
+
+            # if i == 0:
+            #     tmp_map = {x[0] : x[1] for x in zip(cache_sizes, cache_hits)}
+
+            cache_hits = [x[1] / tmp_map[x[0]] for x in zip(cache_sizes, cache_hits)]
+
             pl_line, = plt.plot(cache_sizes, cache_hits, c=col, label=line_name, marker=markers[i])
             plotted.append(pl_line)
 
     plt.xlabel("Cache size")
-    plt.ylabel("Hit ratio")
+    plt.ylabel("Hit ratio compared to best")
 
     plt.legend(handles=plotted)
 
--- src/scripts/plotting/plot_hit_rate_logs.py
+++ src/scripts/plotting/plot_hit_rate_logs.py
@@ -37,9 +37,12 @@ def main():
 
     colors = cm.gnuplot(np.linspace(0, 1, len(txts)))
 
-    fig = plt.figure(1, figsize=(args.size_x, args.size_y))
-    fig.suptitle("Hit rate evaluation")
+    # fig.suptitle("Hit rate evaluation")
 
+    markers = ['^', '*', 'X', '1', '.', 'o', '<', 'v', '2', 's', ',', 'p', 'P' '8', 'h', 'H', '+', '3', 'x', '>',
+               'D', 'd', '|', '4', '_']
+
+    fig = plt.figure(1, figsize=(args.size_x, args.size_y))
     plotted = []
     for file_name, col in zip(txts, colors):
         with open(file_name, "r") as f:
@@ -59,9 +62,39 @@ def main():
     plt.ylabel("Hit ratio")
 
     plt.legend(handles=plotted)
+    plt.tight_layout()
     plt.grid(True)
 
-    plot_name = os.path.join(args.directory, "cache_hit_plot.png")
+    plot_name = os.path.join(args.directory, "cache_hit_plot_cumm.png")
+    plt.plot()
+    plt.savefig(plot_name)
+
+    ################################
+
+    fig = plt.figure(2, figsize=(args.size_x, args.size_y))
+    plotted = []
+    for file_name, col in zip(txts, colors):
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+            line_name = lines[0]
+            lines = lines[1:]
+
+            lines = [x.split() for x in lines]
+
+            cache_sizes = [int(x[1]) for x in lines]
+            cache_hits = [float(x[3]) for x in lines]
+
+            pl_line, = plt.plot(cache_sizes, cache_hits, c=col, label=line_name)
+            plotted.append(pl_line)
+
+    plt.xlabel("Requests processed")
+    plt.ylabel("Hit ratio")
+
+    plt.legend(handles=plotted)
+    plt.tight_layout()
+    plt.grid(True)
+
+    plot_name = os.path.join(args.directory, "cache_hit_plot_inst.png")
     plt.plot()
     plt.savefig(plot_name)
 
